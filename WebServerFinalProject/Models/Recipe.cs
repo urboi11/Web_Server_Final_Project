bb4bfc3a00e274e2dcad5afe73a3edcec5cd9499
Nirupama Poojari <poojarna@mail.uc.edu>
2025-10-25 21:32:09 -0400

    Added a recipe model

---
+++ WebServerFinalProject/Models/Recipe.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebServerFinalProject.Models
+{
+    public class Recipe
+    {
+       public int ID { get; set; }
+        public string Title { get; set; } = null!;
+        public string? Description { get; set; }
+        public string Difficulty { get; set; } = null!;
+        public int PrepMinutes { get; set; }
+        public int BakeMinutes { get; set; }
+        public string Type { get; set; } = null!;
+        public int? CategoryId { get; set; }
+        public Category? Category { get; set; }
+        public ICollection<RecipeIngredient> RecipeIngredients { get; set; } = new List<RecipeIngredient>();
+    }
+}
--- WebServerFinalProject/obj/Release/net9.0/WebServerFinalProject.AssemblyInfo.cs
+++ WebServerFinalProject/obj/Release/net9.0/WebServerFinalProject.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebServerFinalProject")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+523c9fc411d20498869abdac06b455f06024d46c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebServerFinalProject")]

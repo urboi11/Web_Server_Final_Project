2404f33a234d40daee5faaad698a84d904557eae
Nirupama Poojari <poojarna@mail.uc.edu>
2025-11-13 20:29:31 -0500

    Fixed the service classes to be interfaces

---
+++ WebServerFinalProject/Service/IRecipeService.cs
@@ -0,0 +1,15 @@
+﻿using WebServerFinalProject.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WebServerFinalProject.Services
+{
+    public interface IRecipeService
+    {
+        Task<List<Recipe>> GetAllRecipesAsync();
+        Task<Recipe> GetRecipeByIdAsync(int id);
+        Task AddRecipeAsync(Recipe recipe);
+        Task UpdateRecipeAsync(Recipe recipe);
+        Task DeleteRecipeAsync(int id);
+    }
+}

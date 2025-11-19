1e86e5010945c10e056ad96eda651f4abb4491eb
Jason Welsh <132491235+urboi11@users.noreply.github.com>
2025-11-19 17:50:10 -0500

    Adding Edits to Index.cshtml

--- WebServerFinalProject/Controllers/HomeController.cs
+++ WebServerFinalProject/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using WebServerFinalProject.Data;
 using WebServerFinalProject.Models;
 
 namespace WebServerFinalProject.Controllers;
@@ -7,15 +9,22 @@ namespace WebServerFinalProject.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private ApplicationDbContext _dbContext;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, ApplicationDbContext dbContext)
     {
         _logger = logger;
+        _dbContext = dbContext;
     }
 
     public IActionResult Index()
     {
-        return View();
+        List<Recipe> returnedAmount = _dbContext.Recipes.Take(4).ToList();
+        if (returnedAmount.IsNullOrEmpty())
+        {
+            return View();
+        }
+        return View(returnedAmount);
     }
 
     public IActionResult Privacy()
